125983a38d44edcce89e967b0ec2cba9faaef4f3
pe <paul.edouard.boudier@gmail.com>
2017-12-15 14:08:06 +0000

    homepage done need to work on form and brand display

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
     $('.click').on('click', function() {
-        // alert('You clicked the DIV.');
         var link = String(($(this).find('a').attr('href')));
-        /*var test = $(this).find('a');
-        console.log($(test));*/
         window.location.href = link;
     });
     $('#barchart').on('click', function() {
--- resources/views/Quick.blade.php
+++ resources/views/Quick.blade.php
@@ -1,47 +1,121 @@
 @extends('layouts.base')
 @section('title', 'Quick View')
 @section('main')
+    @foreach($brands as $brand)
+        <?php $stock = 0?>
+        @foreach($brand->vehicles as $vehicle)
+            <?php $stock += intval($vehicle->stock) ?>
+        @endforeach
+        <?php $stockTotal += $stock?>
+    @endforeach
     <div class="row quick">
-        <div class="block col-md-3 col-12 click colored">
-            <p>Nb of different brands: {{count($brands->toArray())}}</p>
-            <a href="{{url('/brands')}}" class="fill-div"></a>
+        <div class="block col-md-3 col-12 click colored grow">
+            <div class="row">
+                <div class="col-md-6 col-12">
+                    <p class="count">{{count($brands->toArray())}}</p>
+                    <p>Number of different brands</p>
+                    <a href="{{url('/brands')}}" class="fill-div"></a>
+                </div>
+                <div class="col-md-6 col-12 icon">
+                    <i class="fa fa-pie-chart fa-3x" aria-hidden="true"></i>
+                </div>
+            </div>
         </div>
-        <div class="block col-md-3 col-12 click colored">
-            <p>Nb of different vehicles: {{count($vehicles->toArray())}}</p>
-            @foreach($brands as $brand)
-                <?php $stock = 0?>
-                @foreach($brand->vehicles as $vehicle)
-                    <?php $stock += intval($vehicle->stock) ?>
+        <div class="block col-md-3 col-12 click colored grow">
+            <div class="row">
+                <div class="col-md-6 col-12">
+                    <p class="count">{{count($vehicles->toArray())}}</p>
+                    <p>Nb of different vehicles</p>
+                    <a href="{{url('/vehicles')}}"></a>
+                </div>
+                <div class="col-md-6 col-12 icon">
+                    <i class="fa fa-3x fa-table" aria-hidden="true"></i>
+                </div>
+            </div>
+        </div>
+        <div class="block col-md-3 col-12 click colored grow">
+            <div class="row">
+                <div class="col-md-6 col-12">
+                    <p class="count">{{$stockTotal}}</p>
+                    <p>Actual stock</p>
+                    <a href="{{url('/brands')}}" class="fill-div"></a>
+                </div>
+                <div class="col-md-6 col-12 icon">
+                    <i class="fa fa-cubes fa-3x" aria-hidden="true"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row quick">
+        <div class="block col-md-2 col-12 click colored grow">
+            <div class="row parent-fill">
+                <div class="col-md-6 col-12 center">
+                    <p class="center">Add a vehicle to your garage</p>
+                    <a href="{{url('/create/vehicle')}}"></a>
+                </div>
+                <div class="col-md-6 col-12 icon">
+                    <i class="fa fa-plus fa-3x" aria-hidden="true"></i>
+                </div>
+            </div>
+        </div>
+        <div class="block col-md-8 col-12 chart grow" id="barchart">
+        </div>
+    </div>
+    <div class="row quick">
+        <div class="block col-lg-3 col-md-6 col-12 click colored grow">
+            <h4>Last 5 brands</h4>
+            <table class="table table-sm">
+                <thead>
+                <tr>
+                    <th scope="col">name</th>
+                    <th scope="col">differents cars</th>
+                    <th scope="col">stock</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($l5b as $brand)
+                    <tr>
+                        <td>{{$brand->name}}</td>
+                        <?php
+                            $stock = 0;
+                            $count = 0;
+                        ?>
+                        @foreach($brand->vehicles as $vehicle)
+                            <?php
+                                $stock += intval($vehicle->stock);
+                                $count += 1;
+                            ?>
+                        @endforeach
+                        <td>{{$count}}</td>
+                        <td>{{$stock}}</td>
+                    </tr>
                 @endforeach
-                <?php $stockTotal += $stock?>
-            @endforeach
-            <p>Number of vehicles: {{$stockTotal}}</p>
-            <a href="{{url('/vehicles')}}"></a>
-        </div>
-    </div>
-    <div class="row quick">
-        {{--<div class="block col-md-4 col-12 chart" id="donutchart">
-        </div>--}}
-        <div class="block col-md-4 col-12 click colored">
-            <p>Add a vehicle to your garage</p>
-            <a href="{{url('/create/vehicle')}}"></a>
-        </div>
-        <div class="block col-md-4 col-12 chart" id="barchart">
-        </div>
-    </div>
-    <div class="row quick">
-        <div class="block col-md-3 col-12 click colored">
-            <p>Last 5 brands:</p>
-            @foreach($l5b as $brand)
-                <p>{{$brand->name}}</p>
-            @endforeach
+                </tbody>
+            </table>
             <a href="{{url('/l5b')}}"></a>
         </div>
-        <div class="block col-md-3 col-12 click colored">
-            <p>last models added</p>
-            @foreach($l5v as $vehicle)
-                <p>{{$vehicle->name}}</p>
-            @endforeach
+        <div class="block col-lg-3 col-md-6 col-12 click colored grow">
+            <h4>LAST 5 MODELS</h4>
+            <table class="table table-sm">
+                <thead>
+                <tr>
+                    <th scope="col">name</th>
+                    <th scope="col">brand</th>
+                    <th scope="col">type</th>
+                    <th scope="col">stock</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($l5v as $vehicle)
+                    <tr>
+                        <td>{{$vehicle->name}}</td>
+                        <td>{{$vehicle->brand[0]->name}}</td>
+                        <td>{{$vehicle->type->name}}</td>
+                        <td>{{$vehicle->stock}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
             <a href="{{url('/l5v')}}"></a>
         </div>
     </div>
